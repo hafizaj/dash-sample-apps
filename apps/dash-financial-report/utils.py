23ac2598ceceefd0791f459130b1ae4030527792
Ahmad Hafizuddin Ahmad Jaafar <hafizuddinbinjaafar@gmail.com>
2022-01-13 15:39:08 +0800

    Initialized pages

--- apps/dash-financial-report/utils.py
+++ apps/dash-financial-report/utils.py
@@ -1,5 +1,8 @@
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html, dcc
+
+APP_CONSTANTS = {
+    "graph_color" : '#23359d'
+}
 
 
 def Header(app):
@@ -12,38 +15,27 @@ def get_header(app):
             html.Div(
                 [
                     html.A(
-                        html.Img(
-                            src=app.get_asset_url("dash-financial-logo.png"),
-                            className="logo",
-                        ),
-                        href="https://plotly.com/dash",
-                    ),
-                    html.A(
-                        html.Button(
-                            "Enterprise Demo",
-                            id="learn-more-button",
-                            style={"margin-left": "-10px"},
-                        ),
-                        href="https://plotly.com/get-demo/",
-                    ),
-                    html.A(
-                        html.Button("Source Code", id="learn-more-button"),
-                        href="https://github.com/plotly/dash-sample-apps/tree/main/apps/dash-financial-report",
-                    ),
+                        [
+                            html.Img(
+                            src=app.get_asset_url("logo.png"),
+                            className="logo",)
+                        ],
+                        href="/",
+                    )
                 ],
                 className="row",
             ),
             html.Div(
                 [
                     html.Div(
-                        [html.H5("Calibre Financial Index Fund Investor Shares")],
+                        [html.H5("Business Impact Analysis Dashboard")],
                         className="seven columns main-title",
                     ),
                     html.Div(
                         [
                             dcc.Link(
                                 "Full View",
-                                href="/dash-financial-report/full-view",
+                                href="/full-view",
                                 className="full-view-link",
                             )
                         ],
@@ -64,30 +56,30 @@ def get_menu():
         [
             dcc.Link(
                 "Overview",
-                href="/dash-financial-report/overview",
+                href="/overview",
                 className="tab first",
             ),
             dcc.Link(
                 "Price Performance",
-                href="/dash-financial-report/price-performance",
+                href="/price-performance",
                 className="tab",
             ),
             dcc.Link(
                 "Portfolio & Management",
-                href="/dash-financial-report/portfolio-management",
+                href="/portfolio-management",
                 className="tab",
             ),
             dcc.Link(
-                "Fees & Minimums", href="/dash-financial-report/fees", className="tab"
+                "Fees & Minimums", href="/fees", className="tab"
             ),
             dcc.Link(
                 "Distributions",
-                href="/dash-financial-report/distributions",
+                href="/distributions",
                 className="tab",
             ),
             dcc.Link(
                 "News & Reviews",
-                href="/dash-financial-report/news-and-reviews",
+                href="/news-and-reviews",
                 className="tab",
             ),
         ],
@@ -105,3 +97,145 @@ def make_dash_table(df):
             html_row.append(html.Td([row[i]]))
         table.append(html.Tr(html_row))
     return table
+
+
+def generateRelationshipDict(dframe):
+    depts_relationship = {}
+    for index, row in dframe.iterrows():
+        # Get current row column details
+        curr_core_function = row.CoreFunction
+        curr_other_core_function = row.OtherCoreFunction
+        curr_ipop = row.InputOrOutput
+        # Iterate through the OtherDepts column
+        
+        for r in row.OtherDepts:
+            
+            # If the department already exists in the dictionary, append
+            if r in depts_relationship.keys():
+                for cf in curr_core_function:
+                    if cf not in depts_relationship[r]["CoreFunction"]:
+                        depts_relationship[r]["CoreFunction"].append({
+                            cf: curr_ipop
+                            })
+                for ocf in curr_other_core_function:
+                    if ocf not in depts_relationship[r]["OtherCoreFunction"]\
+                    and ocf!='None':
+                        depts_relationship[r]["OtherCoreFunction"].append({
+                            ocf: curr_ipop
+                            })
+            # Else, add a new    
+            else:
+                depts_relationship[r] = {
+                    'CoreFunction':[{c:curr_ipop} 
+                        for c in curr_core_function],
+                    'OtherCoreFunction': [{c:curr_ipop} 
+                        for c in curr_other_core_function if c!='None']
+                    }
+    return depts_relationship
+
+def getAcronym(word, character_lim=7):
+    '''
+    Creates acronym based on character limit
+    '''
+    if len(word) > character_lim:
+        word = "".join(e[0] for e in word.split())
+    return word
+
+def checkDepartmentEssential(dept, list):
+    '''
+    Classifies a department as core or non-core
+    '''
+
+def generateNodes(data_dictionary, root_label='JPW',
+                  color='#003366'):
+    root_node = {'id': 0, 
+                 'label': root_label ,
+                 'color': '#FFFFFF',
+                 'font': {
+                             'face': 'Helvetica Neue',
+                             'size': 10,
+                             'color': '#000000'
+                            },
+                 }
+    additional_nodes = [{'id': i, 
+                         'label':  getAcronym(key),
+                         'color':color,
+                         'title': key,
+                         'shape': 'circle',
+                         'font': {
+                             'face': 'Helvetica Neue',
+                             'size': 10,
+                             'color': '#FFFFFF'
+                            },
+                         'widthConstraint': {
+                             
+                             }
+                         } 
+                    for i, key in enumerate(data_dictionary)]
+    additional_nodes.append(root_node)
+    return additional_nodes
+
+def generateEdges(data_dictionary, scalefactor = 0.5):
+    '''
+    Generate to and fro edges
+    '''
+    edges = []
+    for i, dept in enumerate(data_dictionary):
+        # Counts for scaling the intensity
+        core_function_count = 0
+        other_function_count = 0
+        # Classifying input and output edges
+        input, output = [], []
+        for cf in data_dictionary[dept]['CoreFunction']:
+            if list(cf.values())[0].lower() == 'input from':
+                input.append(list(cf.keys())[0])
+            else:
+                output.append(list(cf.keys())[0])
+            core_function_count +=1
+        for ocf in data_dictionary[dept]['OtherCoreFunction']:
+            if list(ocf.values())[0].lower() == 'input from':
+                input.append(list(ocf.keys())[0])
+            else:
+                output.append(list(ocf.keys())[0])
+            other_function_count +=1
+        # Check if input or output is empty
+        if not input:
+            pass
+        else:
+            input_from = {
+                'id':f'{i}-0', 
+                'from': i,
+                'to': 0,
+                'title': f'{input}',
+                'arrows': {
+                    'to': {
+                        'enabled' : True,
+                        'scaleFactor': scalefactor
+                    }
+                },
+                'chosen':{
+                    'label': True
+                }
+            }
+            edges.append(input_from)
+        if not output:
+            pass
+        else:
+            output_to = {
+                'id':f'0-{i}', 
+                'from': 0,
+                'to': i,
+                'title': f'{output}',
+                'arrows': {
+                    'to': {
+                        'enabled' : True,
+                        'scaleFactor': scalefactor
+                    }
+                },
+                'chosen':{
+                    'label': True
+                }
+            }
+            edges.append(output_to)
+    return edges
+    
